36b9cb5c02b67d97cbe3b51841553b96b756e868
tkdchen <qcxhome@gmail.com>
2026-09-16 20:40:37 +0800

    Create start-worker.sh

---
+++ hack/start-worker.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+export CONCOURSE_TSA_HOST="127.0.0.1:2222"
+export CONCOURSE_WORK_DIR="/home/cqi/code/cc-ci/workspace"
+export CONCOURSE_TSA_PUBLIC_KEY="/home/cqi/code/cc-ci/keys/tsa_host_key.pub"
+export CONCOURSE_TSA_WORKER_PRIVATE_KEY="/home/cqi/code/cc-ci/keys/worker_key"
+
+concourse worker
