b2d0535475b807a2cc05fa44095b1dd335c42f8f
tkdchen <qcxhome@gmail.com>
2026-09-18 09:16:57 +0800

    Update start-web.sh

--- hack/start-web.sh
+++ hack/start-web.sh
@@ -8,8 +8,10 @@ export CONCOURSE_POSTGRES_DATABASE=concourse
 export CONCOURSE_EXTERNAL_URL="http://localhost:8080"
 export CONCOURSE_ADD_LOCAL_USER="admin:admin"
 export CONCOURSE_MAIN_TEAM_LOCAL_USER=admin
-export CONCOURSE_SESSION_SIGNING_KEY="$HOME/code/cc-ci/keys/session_signing_key"
-export CONCOURSE_TSA_HOST_KEY="$HOME/code/cc-ci/keys/tsa_host_key"
-export CONCOURSE_TSA_AUTHORIZED_KEYS="$HOME/code/cc-ci/keys/authorized_worker_keys"
+export CONCOURSE_SESSION_SIGNING_KEY="/etc/concourse/keys/session_signing_key"
+export CONCOURSE_TSA_HOST_KEY="/etc/concourse/keys/tsa_host_key"
+export CONCOURSE_TSA_AUTHORIZED_KEYS="/etc/concourse/keys/authorized_worker_keys"
+
+export PATH="/usr/local/concourse/bin:$PATH"
 
 concourse web
