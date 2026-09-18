27e5b41fa9ce1ba3e8b23aa3b8160570e4a233c8
tkdchen <qcxhome@gmail.com>
2026-09-18 09:15:43 +0800

    Update start-worker.sh

--- hack/start-worker.sh
+++ hack/start-worker.sh
@@ -5,6 +5,6 @@ export CONCOURSE_WORK_DIR="/var/lib/concourse"
 export CONCOURSE_TSA_PUBLIC_KEY="/etc/concourse/keys/tsa_host_key.pub"
 export CONCOURSE_TSA_WORKER_PRIVATE_KEY="/etc/concourse/keys/worker_key"
 
-export PATH="$HOME/concourse/bin:$PATH"
+export PATH="/usr/local/concourse/bin:$PATH"
 
 concourse worker
