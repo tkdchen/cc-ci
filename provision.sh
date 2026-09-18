a359e5ef8a4071b66e42b763dc8e10127e7458e9
tkdchen <qcxhome@gmail.com>
2026-09-18 09:14:30 +0800

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -1,34 +1,40 @@
 #!/usr/bin/env bash
 
+# Directories:
+# /usr/local/concourse/bin: executables of concourse-ci
+# /etc/concourse/keys: generated keys for running web and worker
+# /var/lib/concourse: concourse data directory
+
 VERSION=0.8.3
 
 fetch_concourse_cli() {
     local -r base_dl_url="https://github.com/concourse/concourse/releases/download/v${VERSION}"
-    mkdir "$HOME/Downloads" || :
-    pushd "$HOME/Downloads"
-    curl -C - -LO "${base_dl_url}/fly-${VERSION}-linux-amd64.tgz"
+    download_dir=$(mktemp -d)
+    pushd "$download_dir"
     curl -C - -LO "${base_dl_url}/concourse-${VERSION}-linux-amd64.tgz"
     tar xvf "concourse-${VERSION}-linux-amd64.tgz"
-    mv concourse/ "$HOME"
-    tar xvf "fly-${VERSION}-linux-amd64.tgz"
-    mkdir "$HOME/.local/bin" || :
-    mv fly "$HOME/.local/bin/"
+    tar xvf concourse/fly-assets/fly-linux-amd64.tgz
+    rm -r concourse/fly-assets/
+    mv fly /usr/local/bin/
+    mv concourse/ /usr/local/
     popd
+    rm -r "$download_dir"
 }
 
-if [ ! -e "$HOME/concourse/" ]; then
+if [ ! -e "/usr/local/concourse/bin/" ]; then
     printf "Fetch concourse CLI\n" >&2
     fetch_concourse_cli
 fi
 
-export PATH="$HOME/concourse/cli:$PATH"
+export PATH="/usr/local/concourse/cli:$PATH"
 
 keys_dir="/etc/concourse/keys"
-[ -e "$keys_dir" ] || sudo mkdir -p "$keys_dir"
+[ -e "$keys_dir" ] || mkdir -p "$keys_dir"
 printf "Set up keys under working directory %s" "$keys_dir" >&2
 [ -e "${keys_dir}/session_signing_key" ] || concourse generate-key -t rsa -f "${keys_dir}/session_signing_key"
 [ -e "${keys_dir}/tsa_host_key" ] || concourse generate-key -t ssh -f "${keys_dir}/tsa_host_key"
 [ -e "${keys_dir}/worker_key" ] || concourse generate-key -t ssh -f "${keys_dir}/worker_key"
+
 cat "${keys_dir}/worker_key.pub" >"${keys_dir}/authorized_worker_keys"
 
-[ -e /var/lib/concourse/ ] || sudo mkdir -p /var/lib/concourse
+[ -e /var/lib/concourse/ ] || mkdir -p /var/lib/concourse
