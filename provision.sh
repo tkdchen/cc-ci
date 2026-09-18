71cc884bb2b6547512321a62fb7f5d964f03b723
tkdchen <qcxhome@gmail.com>
2026-09-18 20:46:54 +0800

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -7,7 +7,7 @@ set -euo pipefail
 # /etc/concourse/keys: generated keys for running web and worker
 # /var/lib/concourse: concourse data directory
 
-VERSION=0.8.3
+export VERSION=0.8.3
 
 fetch_concourse_cli() {
     local -r base_dl_url="https://github.com/concourse/concourse/releases/download/v${VERSION}"
